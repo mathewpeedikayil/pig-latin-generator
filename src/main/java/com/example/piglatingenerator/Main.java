fb973161e1486d5e162ba86707e2415ffef280dc
Mathew Peedikayil <84611340+mathewpeedikayil@users.noreply.github.com>
2023-07-02 15:18:57 +0100

    Formatted main class

--- src/main/java/com/example/piglatingenerator/Main.java
+++ src/main/java/com/example/piglatingenerator/Main.java
@@ -16,7 +16,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class Main extends Application {
