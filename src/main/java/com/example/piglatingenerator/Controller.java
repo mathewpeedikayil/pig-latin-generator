bc2660bb1346dac6db23ffb49e740ab4e98f5fd6
mathewpeedikayil <mathewphilip.peedikayil@gmail.com>
2023-07-02 12:57:43 +0100

    Created and updated controller using show sample controller skeleton option from scene builder

---
+++ src/main/java/com/example/piglatingenerator/Controller.java
@@ -0,0 +1,29 @@
+package com.example.piglatingenerator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class Controller {
+
+    @FXML
+    private Button generatePigLatinButton;
+
+    @FXML
+    private Text pigLatinText;
+
+    @FXML
+    private TextField userInput;
+
+    @FXML
+    void displayPigLatin(MouseEvent event) {
+
+    }
+
+}
+
+
+// Scene Builder
+// View -> Show Sample Controller Skeleton
